5477168d08ab3c40cc7701fc03e272b1f89a0471
antonko3 <125961736+antonko3@users.noreply.github.com>
2023-07-29 18:43:50 +0300

    added -dev

--- src/model/train.py
+++ src/model/train.py
@@ -41,7 +41,8 @@ def get_csvs_df(path):
 def split_data(path):
     from sklearn.model_selection import train_test_split
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0) 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
